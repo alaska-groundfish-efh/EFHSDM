dad8863ee4a3158a2f3b7642a4c4611b651cdfa2
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-03-14 08:56:21 -0700

    last mod to dox before movin to package dev branch

--- R/Functions_Xtable.R
+++ R/Functions_Xtable.R
@@ -11,12 +11,11 @@ rm(rpackages,which_not_installed)
 
 require(xtable)
 require(XML)
-# this is a slightly overcomplicated function, but it automatically adjusts the table to whatever data
-# is supplied to it. Basically, first it makes a lot of notes as to where things should go, then it takes
-# a second pass construct the table and put everything in the right place
-# Known issue: does not calculate the degrees of freedom for factors
-#' Title
+
+#' Make deviance explained table
 #'
+#' @description Make a table of deviance explained for a model object.
+#' @details This is a slightly over-complicated function, but it automatically adjusts the table to whatever data is supplied to it. Basically, first it makes a lot of notes as to where things should go, then it takes a second pass construct the table and put everything in the right place. Known issue: does not calculate the degrees of freedom for factors.
 #' @param model a fitted model object produced by mgcv::gam() or maxnet::maxnet()
 #' @param rsq.method character; a method for the cor() function
 #' @param model.type character; the type of model used, does not autodetect options: maxnet, cloglog, hgam, gam
@@ -28,7 +27,7 @@ require(XML)
 #' @param abund vector of abundance observations
 #' @param train vector of predictions from the model trained ont he full data set
 #' @param test vector of out-of-bag predictions
-#' @param group vector of group labels 
+#' @param group vector of group labels
 #' @param forecast vector of predictions from a third group (not used much anymore)
 #' @param ncoefs vector of coefficients used in maxnet models
 #' @param devs vector of deviance explained estimates
@@ -54,11 +53,11 @@ MakeXtable<-function(model,             # a model object
                      ncoefs=NULL,       # a named vector of coefficients from the MaxnetStats function
                      devs=NULL,         # a named vector of deviance %s from one of the ___Stats functions
                      area=NULL){        # a single value for the area of the EFH
-  
+
   dim.names1<-""
   dim.names2<-"Model term"
   if(length(devs)>0){if(is.na(devs)){devs<-NULL}}
-  
+
   if(tolower(model.type)=="maxnet"){
     if(is.null(ncoefs)==F){
       dim.names1<-c(dim.names1,"")
@@ -81,7 +80,7 @@ MakeXtable<-function(model,             # a model object
       term.order<-order(devs,ncoefs,decreasing = T)
       edf.order<-term.order
     }
-    
+
     dim.names1<-c(dim.names1,"")
     dim.names2<-c(dim.names2,"Total Deviance Explained")
     tot.dev<-format(signif(max(model$dev.ratio)*100,3),nsmall=1)
@@ -90,10 +89,10 @@ MakeXtable<-function(model,             # a model object
     # it'll be helpful to keep the names attached
     term.table<-AutodetectGAMTerms(model,hgam="d")
     term.table<-subset(term.table,type!="offset")
-    
+
     dim.names1<-c(dim.names1,"")
     dim.names2<-c(dim.names2,"EDF")
-    
+
     # a little work to make sure all the names and stats match up
     nterms <- round(summary(model)$s.table[1:nrow(term.table[term.table$type!="factor",]),1],1)
     edf.names<-vector(length=length(nterms))
@@ -102,7 +101,7 @@ MakeXtable<-function(model,             # a model object
       name1<-strsplit(name0[2],split=",")[[1]]
       edf.names[n]<-paste(name1,collapse="*")
     }
-    
+
     if(is.null(devs)==F){
       dev.col=length(dim.names1)+1
       dim.names1<-c(dim.names1,"")
@@ -116,9 +115,9 @@ MakeXtable<-function(model,             # a model object
     dim.names1<-c(dim.names1,"")
     dim.names2<-c(dim.names2,"Total Deviance Explained")
     totdev.col<-length(dim.names1)
-    
+
     tot.dev<-format(signif(summary(model)$dev.expl*100, 3),nsmall=1)
-    
+
     #check that edf is in the same order as the devs
     edf.order<-match(edf.names,names(devs))
   }
@@ -128,19 +127,19 @@ MakeXtable<-function(model,             # a model object
     train.col<-c(length(dim.names1)+1,length(dim.names1)+2)
     dim.names1<-c(dim.names1,"Training data","Training data")
     dim.names2<-c(dim.names2,"rsq","rmse")
-    
+
     if(is.null(efh.break)==F){
       obs.efh<-as.integer(abund>efh.break)
       pred.efh<-as.integer(train>efh.break)
-      
+
       n.nas<-sum(is.na(pred.efh))
       denom<-length(obs.efh)-n.nas
-      
+
       t.pos<-round(sum(as.integer(obs.efh==1 & pred.efh==1),na.rm=T)/denom*100,1)
       t.neg<-round(sum(as.integer(obs.efh==0 & pred.efh==0),na.rm=T)/denom*100,1)
       f.pos<-round(sum(as.integer(obs.efh==0 & pred.efh==1),na.rm=T)/denom*100,1)
       f.neg<-round(sum(as.integer(obs.efh==1 & pred.efh==0),na.rm=T)/denom*100,1)
-      
+
       train.table<-array(dim=c(7,3),data="")
       train.table[,1]<-c("Training Data","","Obs. EFH","Obs. non-EFH","","# Samples","Total Accuracy %")
       train.table[,2]<-c("","Pred. EFH",t.pos,f.pos,"",length(train),t.pos+t.neg)
@@ -152,19 +151,19 @@ MakeXtable<-function(model,             # a model object
     test.col<-c(length(dim.names1)+1,length(dim.names1)+2)
     dim.names1<-c(dim.names1,"Test data","Test data")
     dim.names2<-c(dim.names2,"rsq","rmse")
-    
+
     if(is.null(efh.break)==F){
       obs.efh<-as.integer(abund>efh.break)
       pred.efh<-as.integer(test>efh.break)
-      
+
       n.nas<-sum(is.na(pred.efh))
       denom<-length(obs.efh)-n.nas
-      
+
       t.pos<-round(sum(as.integer(obs.efh==1 & pred.efh==1),na.rm=T)/denom*100,1)
       t.neg<-round(sum(as.integer(obs.efh==0 & pred.efh==0),na.rm=T)/denom*100,1)
       f.pos<-round(sum(as.integer(obs.efh==0 & pred.efh==1),na.rm=T)/denom*100,1)
       f.neg<-round(sum(as.integer(obs.efh==1 & pred.efh==0),na.rm=T)/denom*100,1)
-      
+
       test.table<-array(dim=c(7,3),data="")
       test.table[,1]<-c("CV Testing Data","","Obs. EFH","Obs. non-EFH","","# Samples","Total Accuracy %")
       test.table[,2]<-c("","Pred. EFH",t.pos,f.pos,"",length(test),t.pos+t.neg)
@@ -191,14 +190,14 @@ MakeXtable<-function(model,             # a model object
   if(is.null(nice.names)==F){
     term.names<-nice.names[match(term.names,nice.names[,1]),2]
   }
-  
+
   # now we have a set of everything we need, so make the table and start filling it in
   table1 <- array("", dim = c(length(term.names)+2,length(dim.names1)))
   table1[1,] <- dim.names1
   table1[2,] <- dim.names2
   table1[3:(length(term.names)+2),1]<-term.names[term.order]
   table1[3,totdev.col]<-tot.dev
-  
+
   if(is.null(ncoefs)==F | model.type!="maxnet"){
     table1[3:(length(term.names)+2),2]<-nterms[term.order]
     table1[is.na(table1[,2]),2]<-1
@@ -208,7 +207,7 @@ MakeXtable<-function(model,             # a model object
     train.dat<-na.omit(train)
     table1[3,train.col[1]]<-round(cor(abund,train,method=rsq.method)^2,3)
     table1[3,train.col[2]]<-format(round(RMSE(pred = train,obs = abund),1),nsmall=1,big.mark = ",")
-    
+
   }
   if(is.null(test)==F){
     test.dat<-na.omit(test)
@@ -221,7 +220,7 @@ MakeXtable<-function(model,             # a model object
     table1[3,forecast.col[2]]<-format(round(RMSE(pred = forecast,obs = abund),1),nsmall=1,big.mark = ",")
   }
   if(is.null(area)==F){table1[3,area.col]<-format(round(area,-2),big.mark = ",")}
-  
+
   if(is.null(model$family$family)==F && model$family$family=="quasipoisson"){
     theta.table<-array("",dim=c(2,length(dim.names1)))
     theta.table[1,1]<-"."
@@ -236,29 +235,29 @@ MakeXtable<-function(model,             # a model object
     theta.table[2,2]<-round(as.numeric(strsplit(model$family$family,split="[()]")[[1]][2]),3)
     table1<-rbind(table1,theta.table)
   }
-  
+
   if(model.type=="maxnet" & is.numeric(regmult)){
     reg.table<-array("",dim=c(2,length(dim.names1)))
     reg.table[1,1]<-"."
     reg.table[2,1:2]<-c("Regularization Constant",format(round(regmult,1),nsmall=1))
     table1<-rbind(table1,reg.table)
   }
-  
+
   if(is.numeric(scale.factor) & scale.factor!=1){
     scale.table<-array("",dim=c(2,length(dim.names1)))
     scale.table[1,1]<-"."
     scale.table[2,1:2]<-c("Scale Factor",format(round(scale.factor,2),nsmall=1))
     table1<-rbind(table1,scale.table)
   }
-  
+
   # make a spacer so the table is formatted nice and readable, if necessary
   spacer<-NULL
   if(is.na(group)==F & is.null(group)==F | is.null(train)==F |is.null(test)==F){
     spacer<-array("",dim=c(2,length(dim.names1)))
     spacer[,1]<-"."
   }
-  
-  
+
+
   # now go and add in a table for the confusion matrices
   acc.table<-NULL
   if(is.null(train)==F |is.null(test)==F & is.null(efh.break)==F){
@@ -273,11 +272,11 @@ MakeXtable<-function(model,             # a model object
       acc.table[2:8,1:3]<-test.table
     }
   }
-  
+
   cv.table<-NULL
   #now, add in a table for the cross-validation info
   if(is.na(group)==F & is.null(group)==F){
-    
+
     cv.rows<-length(unique(group))+3
     cv.table<-array("",dim = c(cv.rows,length(dim.names1)))
     cv.table[1,1]<-"Cross Validation Results"
@@ -303,7 +302,7 @@ MakeXtable<-function(model,             # a model object
     }
   }
   model.table <- xtable::xtable(rbind(table1,spacer,acc.table,spacer,cv.table))
-  
+
   xtable::print.xtable(model.table, type = "html", file = filename,
                        include.rownames = getOption("xtable.include.rownames", FALSE), html.table.attributes = 2,
                        include.colnames = getOption("xtable.include.colnames", FALSE), hline.after = getOption("xtable.hline.after",
@@ -311,10 +310,11 @@ MakeXtable<-function(model,             # a model object
 }
 
 
-# For now, you need to include each of the elements for the ensemble table to work, so no shortcuts
-# kind of cumbersome and unlikely to be useful for others
-#' Title
+
+#' Make deviance explained table (for ensemble)
 #'
+#' @description Make an HTML table of deviance explained for a model ensemble.
+#' @details For now, you need to include each of the elements for the ensemble table to work, so no shortcuts. It is kind of cumbersome and unlikely to be useful for others. May soon be deprecated.
 #' @param model.names vector of names for the models
 #' @param ensemble logical; is an ensemble included
 #' @param weights vector of weights for each model other than the ensemble
@@ -332,20 +332,20 @@ MakeXtable<-function(model,             # a model object
 #'
 #' @examples
 MakeEnsembleXtable<-function(model.names=c("maxnet","cloglog","hpoisson","poisson","negbin"),
-                             ensemble=T, 
+                             ensemble=T,
                              weights=NULL,
                              converge.vec=NULL,
                              abund.check.vec=NULL,
-                             cor.method="pearson",       # 
+                             cor.method="pearson",       #
                              preds.table=NULL,
                              scale.facs=NULL,
                              efh.breaks=NULL,
                              areas=NULL,
                              filename){
-  
+
   # this part detects what data are available and formats things appropriately, sturdy to missing data
   dim.names<-"Model"
-  
+
   if(sum(c(is.null(preds.table)==F,is.na(preds.table)==F))>1){
     N.col<-2
     dim.names<-c(dim.names,"N")
@@ -374,21 +374,21 @@ MakeEnsembleXtable<-function(model.names=c("maxnet","cloglog","hpoisson","poisso
     break.col<-length(dim.names)+1
     dim.names<-c(dim.names,"EFH break")
   }
-  
+
   if(is.numeric(areas)){
     area.col<-length(dim.names)+1
     dim.names<-c(dim.names,"Area")
   }
-  
+
   n.models<-length(model.names)
   model.rows<-2:(n.models+1)
-  
+
   etable<-array("",dim=c(3+n.models,length(dim.names)))
-  
+
   # now go back and fill the rows in
   etable[,1]<-c("Model",model.names,".","ensemble")
   etable[1,]<-dim.names
-  
+
   if(sum(c(is.null(preds.table)==F,is.na(preds.table)==F))>1){
     N.vec<-rep(NA,length(model.names))
     cor.vec<-rep(NA,length(model.names))
@@ -401,9 +401,9 @@ MakeEnsembleXtable<-function(model.names=c("maxnet","cloglog","hpoisson","poisso
         pred.dat$pred[which(is.infinite(pred.dat$pred))]<-10^10
         pred.cvpred<-ifelse(RMSE(pred = pred.dat$pred,obs = pred.dat$abund)<
                               RMSE(pred = pred.dat$cvpred,obs = pred.dat$abund),"cvpred","pred")
-        
+
         prob.cvprob<-ifelse(pred.cvpred=="cvpred","cvprob","prob")
-        
+
         N.vec[m]<-sum(pred.dat$abund>0,na.rm=T)
         rmse.vec[m]<-RMSE(pred = pred.dat[,pred.cvpred],obs = pred.dat$abund)
         cor.vec[m]<-cor(pred.dat$abund,round(pred.dat[,pred.cvpred],2),method=cor.method)
@@ -411,18 +411,18 @@ MakeEnsembleXtable<-function(model.names=c("maxnet","cloglog","hpoisson","poisso
         pde.vec[m]<-PDE(obs = pred.dat$abund,pred = pred.dat[,pred.cvpred])
       }
     }
-    
+
     digs<-ifelse(min(rmse.vec,na.rm=T)>1000,0,ifelse(min(rmse.vec,na.rm = T)<10,2,1))
-    
+
     etable[model.rows,N.col]<-N.vec
     etable[model.rows,pred.cols[1]]<-format(round(rmse.vec,digs),nsmall=digs,big.mark = ",")
     etable[model.rows,pred.cols[2]]<-format(round(cor.vec, 2),nsmall=2)
     etable[model.rows,pred.cols[3]]<-format(round(auc.vec, 2),nsmall=2)
     etable[model.rows,pred.cols[4]]<-format(round(pde.vec, 2),nsmall=2)
-    
+
     if(ensemble){
       ensemble.dat<-na.omit(subset(preds.table,Model=="ensemble"))
-      
+
       etable[n.models+3,N.col]<-sum(ensemble.dat$abund>0,na.rm=T)
       etable[n.models+3,pred.cols[1]]<-format(round(RMSE(obs=ensemble.dat$abund,pred=ensemble.dat$pred),digs),nsmall=digs,big.mark = ",")
       etable[n.models+3,pred.cols[2]]<-format(round(cor(ensemble.preds$abund,ensemble.preds$pred,method=cor.method), 2),nsmall=2)
@@ -478,7 +478,7 @@ MakeEnsembleXtable<-function(model.names=c("maxnet","cloglog","hpoisson","poisso
     etable[model.rows,area.col]<-format(round(area.vec,-2),big.mark = ",")
     if(ensemble){etable[n.models+3,area.col]<-format(round(areas[which(names(areas)=="ensemble")],-2),big.mark = ",")}
   }
-  
+
   etable[trimws(etable)=="NA"]<-"--"
 
   ensemble.table <- xtable::xtable(etable)
@@ -491,15 +491,28 @@ MakeEnsembleXtable<-function(model.names=c("maxnet","cloglog","hpoisson","poisso
 
 
 
+#' Make deviance table
+#'
+#' @param model.names
+#' @param model.types
+#' @param nice.names
+#' @param dev.list
+#' @param model.weights
+#' @param filename
+#'
+#' @return
+#' @export
+#'
+#' @examples
 MakeDevianceTable<-function(model.names=NULL,
                             model.types=NULL,
                             nice.names=NULL,
                             dev.list,
                             model.weights=NULL,
                             filename){
-  
-  
-  
+
+
+
   if(is.null(model.weights)||is.na(model.weights)){
     good.models<-1:length(dev.list)
   }else{
@@ -509,7 +522,7 @@ MakeDevianceTable<-function(model.names=NULL,
     model.names<-paste0("Model ",1:length(dev.list))
   }
   good.names<-model.names[good.models]
-  
+
   columns<-"Model"
   if(is.null(model.types)==F&&is.na(model.types)==F){
     t.col<-2
@@ -519,28 +532,28 @@ MakeDevianceTable<-function(model.names=NULL,
     w.col<-length(columns)+1
     columns<-c(columns,"Weight")
   }
-  
+
   dev.names<-unique(names(unlist(dev.list)))
   dev.names<-dev.names[dev.names!=""]
   n.terms<-length(dev.names)
-  
+
   # if names aren't supplied, use the terms from the model
   if(is.null(nice.names)){
     nice.names<-data.frame(var=dev.names,name=dev.names)
   }
-  
+
   model.rows<-2:(length(good.models)+1)
   term.col<-(length(columns)+1):(length(columns)+n.terms)
-  
+
   columns<-c(columns,nice.names$name[match(dev.names,nice.names$var)])
-  
+
   dtable<-array(dim=c(length(good.names)+2,length(columns)))
-  
+
   dtable[1,]<-c(columns)
-  
+
   for(d in 1:length(good.models)){
     dtable[1+d,1]<-good.names[d]
-    
+
     if("Type"%in%columns){
       dtable[1+d,2]<-model.types[good.models[d]]
     }
@@ -559,51 +572,51 @@ MakeDevianceTable<-function(model.names=NULL,
       if(is.na(dtable[(1+d),term.col[t]])){dtable[(1+d),term.col[t]]<-"--"}
     }
   }
-  
+
   dtable[nrow(dtable),1]<-"Mean"
-  
+
   mean.matrix<-matrix(nrow=length(model.rows),ncol=length(term.col),data = as.numeric(dtable[model.rows,term.col]),byrow = F)
-  
+
   term.means<-round(apply(mean.matrix,MARGIN = 2,FUN = mean,na.rm=T),1)
-  
+
   dtable[nrow(dtable),term.col]<-term.means
   term.order<-order(term.means,decreasing = T)
-  
+
   dtable2<-dtable
   dtable2[,term.col]<-dtable[,term.col[term.order]]
-  
+
   if("Weight"%in%columns){
     w.mean<-array(dim=c(1,ncol(dtable2)))
-    
+
     term.means2<-round(apply(mean.matrix*model.weights[good.models],MARGIN = 2,FUN = sum,na.rm=T),1)
-    
+
     term.order<-order(term.means2,decreasing = T)
-    
+
     dtable2<-dtable
     dtable2[,term.col]<-dtable[,term.col[term.order]]
-    
+
     w.mean[1,1]<-"Weighted Mean"
     w.mean[,term.col]<-term.means2[term.order]
-    
+
     dtable2<-rbind(dtable2,w.mean)
   }
-  
+
   deviance.table <- xtable::xtable(dtable2)
   xtable::print.xtable(deviance.table, type = "html", file = filename,
                        include.rownames = getOption("xtable.include.rownames", FALSE), html.table.attributes = 2,
                        include.colnames = getOption("xtable.include.colnames", FALSE), hline.after = getOption("xtable.hline.after",
                                                                                                                c(-1, 1, nrow(deviance.table))))
-  
+
   # Make a second table
-  
+
   rescale.col2<-as.numeric(dtable2[nrow(dtable2),4:ncol(dtable2)])
   rescale.col3<-cumsum(dtable2[nrow(dtable2),4:ncol(dtable2)])
-  
+
   dtable3<-array(dim=c(1+length(term.means),3))
   dtable3[,1]<-c("Covariate",dtable2[1,4:ncol(dtable2)])
   dtable3[,2]<-c("% Contribution",format(round(100*rescale.col2/sum(rescale.col2),1),nsmall=1))
   dtable3[,3]<-c("Cumulative %",format(round(100*rescale.col3/sum(rescale.col2),1),nsmall=1))
-  
+
   deviance.table2 <- xtable::xtable(dtable3)
   xtable::print.xtable(deviance.table2, type = "html", file = paste0(strsplit(filename,split = ".html")[[1]],"2.html"),
                        include.rownames = getOption("xtable.include.rownames", FALSE), html.table.attributes = 2,
