111222a5b4545e2220e7f88f35717569eeb14863
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-03-14 08:46:30 -0700

    dev skeleton

--- dev/dev.R
+++ dev/dev.R
@@ -1,8 +1,23 @@
 # Scripts use to run pkg development
 
-# Documentation
+
+# Documentation -----------------------------------------------------------
+# Package-level documentation
+use_package_doc(open = rlang::is_interactive())
+
+# Function documentation
 devtools::document()
 
+# Vignettes
+usethis::use_vignette("EFHSDM")
+devtools::build_vignettes()
 
-# Dependencies - need to add everything that gets called w library() or require() here.
+
+# Dependencies ------------------------------------------------------------
+# ***Need to add everything that gets called w library() or require() here.
 use_package(package, type = "Imports", min_version = NULL)
+
+
+# Datasets ----------------------------------------------------------------
+
+use_data() #FILL IN with example datasets that need to be stored with the pkg
