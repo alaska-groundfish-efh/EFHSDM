e182779b642c575804d8195819fb61bbeaaa1b6f
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2025-04-16 10:35:31 -0700

    suppress akgfmaps message

--- tests/testthat/test-MakeAKGFDotplot.R
+++ tests/testthat/test-MakeAKGFDotplot.R
@@ -3,13 +3,16 @@ test_that("MakeAKGFDotPlot() creates a ggplot object", {
   species.data <- readRDS(system.file("test_files", "goa_data_logarea_folds.rds", package = "EFHSDM"))
   data("raster_stack", package = "EFHSDM")
   hd <- quantile(species.data[species.data[, "dogfish"] > 0, "dogfish"], probs = .9)
-  testfig <- MakeAKGFDotplot(
+
+  testfig <- suppressMessages( # suppress akgfmaps message about EPSG:3338
+    MakeAKGFDotplot(
     presence = species.data[species.data[, "dogfish"] > 0, ],
     absence = species.data[species.data[, "dogfish"] == 0, ],
     highdensity = species.data[species.data[, "dogfish"] > hd, ],
     region = tolower("GOA"),
     dataCRS = raster::crs(raster_stack, asText = TRUE),
     title.name = "This figure is a test"
+  )
   )
   expect_true(is(testfig, "ggplot"))
 })
