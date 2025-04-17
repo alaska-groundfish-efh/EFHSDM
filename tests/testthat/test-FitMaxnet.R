0b820fabaadce7b73cda2d1146ca413489623e97
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2025-04-16 18:40:14 -0700

    comment out unfinished end-to-end test

--- tests/testthat/test-FitMaxnet.R
+++ tests/testthat/test-FitMaxnet.R
@@ -1,4 +1,4 @@
-test_that("FitMaxnet() creates object of proper length", {
+test_that("FitMaxnet() creates object with class of expected length", {
   # This is basic but will be useful if package dependencies change
   species.data <- readRDS(system.file("test_files", "goa_data_logarea_folds.rds", package = "EFHSDM"))
   maxnet.covars <- c(
@@ -14,25 +14,6 @@ test_that("FitMaxnet() creates object of proper length", {
   )
 
   expect_length(object = class(maxnet.model0), n = 3)
-})
-
-# Need to check that gets the same outputs as in the 2023 5-Year EFH cycle
-# This is species-specific and right now it uses GOA dogfish.
-test_that("FitMaxnet() object matches 2023 review cycle", {
-  species.data <- readRDS(system.file("test_files", "goa_data_logarea_folds.rds", package = "EFHSDM"))
-  maxnet.covars <- c(
-    "bcurrentU", "bcurrentV", "bcurrentUSD", "bcurrentVSD", "bdepth",
-    "slope", "aspectE", "aspectN", "curve", "btemp", "tmax", "rocky", "BPI"
-  )
-  cofactors <- c("sponge", "coral", "pen")
-  r.mult <- 0.5
-  maxnet.model0 <- FitMaxnet(
-    data = species.data, species = "dogfish", vars = maxnet.covars,
-    facs = cofactors,
-    regmult = r.mult, reduce = T
-  )
-  maxnet.model0_check <- readRDS(system.file("test_files", "goa_maxnet_model0_test.rds", package = "EFHSDM"))
-  expect_true(identical(maxnet.model0, maxnet.model0_check))
 })
 
 
@@ -120,3 +101,24 @@ test_that("FitMaxnet works with regmult set", {
   expect_s3_class(model, "maxnet")
   # Optional: check some structure of the model if regmult affects it
 })
+
+
+# Need to check that gets the same outputs as in the 2023 5-Year EFH cycle
+# This is species-specific and right now it uses GOA dogfish.
+# test_that("FitMaxnet() object matches 2023 review cycle", {
+#   species.data <- readRDS(system.file("test_files", "goa_data_logarea_folds.rds", package = "EFHSDM"))
+#   maxnet.covars <- c(
+#     "bcurrentU", "bcurrentV", "bcurrentUSD", "bcurrentVSD", "bdepth",
+#     "slope", "aspectE", "aspectN", "curve", "btemp", "tmax", "rocky", "BPI"
+#   )
+#   cofactors <- c("sponge", "coral", "pen")
+#   r.mult <- 0.5
+#   maxnet.model0 <- FitMaxnet(
+#     data = species.data, species = "dogfish", vars = maxnet.covars,
+#     facs = cofactors,
+#     regmult = r.mult, reduce = T
+#   )
+#   maxnet.model0_check <- readRDS(system.file("test_files", "goa_maxnet_model0_test.rds", package = "EFHSDM"))
+#   expect_true(identical(maxnet.model0, maxnet.model0_check))
+# })
+
